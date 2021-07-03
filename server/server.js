6afd838e4c0f1dae99e1754cc10fcf6a7b27dfa5
Chapler1 <wjc23@pitt.edu>
2021-07-02 21:08:39 -0400

    fix

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express')
-const spotifyWebApi = require('spotify-web-api-node')
+const express = require('express');
+const spotifyWebApi = require('spotify-web-api-node');
 const app = express();
 
-app.post('/login', function(req, res) => {
+app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
         redirectUri: 'http://localhost:3000',
-        clientId: 'eba98dc6511f48a080c04e21836548b5'
-        clientSecret: '1feb253ec4e940e8a4e281093aebe369'
+        clientId: 'eba98dc6511f48a080c04e21836548b5',
+        clientSecret: '1feb253ec4e940e8a4e281093aebe369',
     })
 
     spotifyApi.authorizationCodeGrant(code)
