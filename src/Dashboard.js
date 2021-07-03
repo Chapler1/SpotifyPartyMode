fcce9573ebf7a817df70858c0b840719dcfc2ea2
Chapler1 <wjc23@pitt.edu>
2021-07-03 19:02:51 -0400

    search

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,7 +1,32 @@
- import React from "react"
+ import { useState, useEffect } from "react"
  import useAuth from './useAuth'
+ import { Container, Form } from "react-bootstrap"
+ import SpotifyWebApi from "spotify-web-api-node"
+
+ const spotifyApi = new SpotifyWebApi({
+     clientId: 'eba98dc6511f48a080c04e21836548b5',
+ })
 
  export default function Dashboard({code}) {
      const accessToken = useAuth(code)
-     return <div>{code}</div>
+     const [search, setSearch] = useState("")
+     const [searchResults, setSearchResults] = useState([])
+
+     useEffect(() => {
+         if (!accessToken) return
+        spotifyApi.setAccessToken(accessToken)
+     }, [accessToken])
+
+     useEffect(() => {
+        if (!search) return setSearchResults([])
+        if (!accessToken) return
+
+        spotifyApi.searchTracks(search).then(res =>  {
+            
+        })
+     } [search, accessToken])
+
+     return <Container>
+         <Form.Control type="search" placeholder="Search for Artists/Songs" value={search} onChange={e => setSearch(e.target.value)} />
+     </Container>
  }
--- src/useAuth.js
+++ src/useAuth.js
@@ -12,7 +12,7 @@ export default function useAuth(code) {
         }).then(res => {
             setAccessToken(res.data.accessToken)
             setRefreshToken(res.data.refreshToken)
-            setExpiresIn(61)
+            setExpiresIn(res.data.expiresIn)
             window.history.pushState({}, null, '/')
         }).catch(() => { 
             window.location = '/'
@@ -21,19 +21,19 @@ export default function useAuth(code) {
 
     useEffect(() => {
         if (!refreshToken || !expiresIn) return
-        const timeout = setTimeout(() => {
+        const interval = setInterval(() => {
 
     axios.post('http://localhost:3001/refresh', {
             refreshToken,
         }).then(res => {
             setAccessToken(res.data.accessToken)
-            setExpiresIn(61)
+            setExpiresIn(res.data.expiresIn)
         }).catch(() => { 
             window.location = '/'
         })
     }, (expiresIn - 60) * 1000)
 
-    return () => clearTimeout(timeout) 
+    return () => clearInterval(interval) 
     }, [refreshToken, expiresIn])
 
     return accessToken
