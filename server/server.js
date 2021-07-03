5723bf13e4e753338da7ab205fc1a11e6e7a010f
Chapler1 <wjc23@pitt.edu>
2021-07-03 18:34:29 -0400

    refreshAuth

--- server/server.js
+++ server/server.js
@@ -7,6 +7,24 @@ const app = express()
 app.use(cors())
 app.use(bodyParser.json())
 
+app.post('/refresh', (req, res) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri: 'http://localhost:3000',
+        clientId: 'eba98dc6511f48a080c04e21836548b5',
+        clientSecret: '1feb253ec4e940e8a4e281093aebe369',
+        refreshToken,
+    })
+    spotifyApi.refreshAccessToken().then(data => {
+            res.json({
+                accessToken: data.body.accessToken, expiresIn: data.body.expiresIn
+            })
+        })
+          .catch(() => {
+              res.sendStatus(400)
+          })
+})
+    
 app.post('/login', (req, res) => {
     const code = req.body.code
     const spotifyApi = new SpotifyWebApi({
